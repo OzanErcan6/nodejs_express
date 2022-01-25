5493b1a730d24fa965e566c49c37687f9ea617a4
Ozan Ercan <ozanercann@gmail.com>
2022-01-25 22:27:25 +0300

    callback hell fixed

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,44 +1,26 @@
 // insert find remove and update operations
 
-const assert = require('assert')
+const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
-    const coll = db.collection(collection)
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null)
-        // how many documents inserted
-        console.log("Inserted " + result.result.n + " documents into the collection " + collection ) 
-        // receive result as callback result
-        callback(result)
-    })
-}
+    const coll = db.collection(collection);
+    return coll.insert(document);
+};
 
 exports.findDocuments = (db, collection, callback) => {
-    const coll = db.collection(collection)
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null)
-        //retrieve back docs to the callee function
-        callback(docs)
-    })
-}
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();
+};
 
-exports.removeDocument = (db,document, collection, callback) => {
-    const coll = db.collection(collection)
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null)
-        console.log("Removed the document", document)
-        callback(result)
-    })
-}
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);
+};
 
 exports.updateDocument = (db, document, update, collection, callback) => {
-    const coll = db.collection(collection)
-    coll.updateOne(document, { $set: update}, null, (err, result) => {
-        assert.equal(err, null)
-        console.log("updated the document with ", update)
-        callback(result)
-    })
-}
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
+};
 
 
 
