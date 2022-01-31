41fd37c4812e119c90f1df3cc433558764a22266
Ozan Ercan <ozanercann@gmail.com>
2022-01-31 20:30:00 +0300

    mongoose part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,12 +8,10 @@ const connect = mongoose.connect(url)
 connect.then((db) => {
     console.log('connected correctly to the server')
     
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    })
-
-    newDish.save()
+        })
         .then((dish) => {
             console.log(dish)
             return Dishes.find({}).exec()
