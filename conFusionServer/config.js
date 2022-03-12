db51c766e59bb22423baecb92b5f2d493493223e
OzanErcan6 <ozanercann@gmail.com>
2022-03-13 02:21:41 +0300

    facebook auth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '325557382751350',
+        clientSecret: 'e9b1f753ad550c85c87d418aa1c08a02'
+    }
 }
